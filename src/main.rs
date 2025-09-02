cc533b8c13db15a6fcbbccfc6e560c8fdba74ae2
Alexander Bondarenco <sasha.199568@gmail.com>
2025-09-02 14:29:50 +0300

    Vector

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+
+use std::ops::Add;
+
+struct Vector {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+macro_rules! vector {
+    ($x:expr, $y:expr, $z:expr) => {
+        Vector { x: $x as f64, y: $y as f64, z: $z as f64 }
+    };
+}
+
+impl Vector {
+
+    fn print(&self) {
+        println!("Vector({} {} {})", self.x, self.y, self.z);
+    }
+}
+
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        vector!(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+
 fn main() {
-    let x = 5;
-    println!("x is {x}");
+    let a = vector!(1, 2, 3);
+    let b = vector!(1, 2.3, 4);
+    let sum = a + b;
+    sum.print();
 }
